df0e99746e3fe6bb6f67e551dd38f4deedb0cce7
t-tullis <t.tullis34@gmail.com>
2020-02-25 16:57:08 -0800

    implemented node and singly linked list class

---
+++ Singly-Linked-List.js
@@ -0,0 +1,16 @@
+    //Creates Nodes
+    class Node{
+        constructor(val){
+            this.val = val
+            this.next = null
+        }
+    }
+
+    //Creates a linked list
+    class SinglyLinkedList{
+        constructor(){
+            this.head = null;
+            this.tail = null;
+            this.length = 0;
+        }
+    }
