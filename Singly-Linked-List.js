3a341c1765a5271145b57a70422a43134ce1a630
t-tullis <t.tullis34@gmail.com>
2020-02-26 16:58:56 -0800

    added push/add method to singly linked list

--- Singly-Linked-List.js
+++ Singly-Linked-List.js
@@ -13,4 +13,17 @@
             this.tail = null;
             this.length = 0;
         }
+
+        push(val){
+            let newNode = new Node(val)
+            if(!this.head){
+                this.head = newNode
+                this.tail = this.head
+            }else{
+                this.tail.next = newNode
+                this.tail = newNode
+            }
+            this.length++
+            return this
+        }
     }
