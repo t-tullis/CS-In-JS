767d5dc7b2613cdeb2beaf47838f1db7c4e79d3a
t-tullis <t.tullis34@gmail.com>
2020-02-27 17:00:40 -0800

    added pop/remove method for singly linked list

--- Singly-Linked-List.js
+++ Singly-Linked-List.js
@@ -26,4 +26,26 @@
             this.length++
             return this
         }
+        pop(){
+            if(!this.head){
+                return undefined
+            }
+            //current and newTail is set to head node
+            let current = this.head;
+            let newTail = current
+            //while current.next isn't null keep looping
+            while(current.next != null){
+                //set new tail to current
+                newTail = current;
+                //set current to next node in list
+                current = current.next;
+            }
+            //set the tail = to the newTail
+            this.tail = newTail;
+            //set the tails next to null to reassign tail of list
+            this.tail.next = null;
+            //decrement the length of the list to show popped value was removed.
+            this.length--
+            return current
+        }
     }
